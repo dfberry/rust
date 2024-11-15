7070a628f6191dccf8d5d4085d7b3ce074e1191d
Dina Berry <dinaberry@outlook.com>
2024-11-15 16:33:26 +0000

    async

--- crates/crate_file_io/src/main.rs
+++ crates/crate_file_io/src/main.rs
@@ -1,13 +1,13 @@
 use serde_json::json;
 use serde_json::Value;
-use std::fs::OpenOptions;
-use std::io::{Read, Write};
+use tokio::fs::OpenOptions;
+use tokio::io::AsyncWriteExt;
 use tokio;
 
 pub async fn write_json_to_file(
     file_name_and_path: &str, 
     json_blob: &Value
-) -> Result<(), std::io::Error> {
+) -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Writing JSON to file: {}", file_name_and_path);
 
@@ -15,18 +15,18 @@ pub async fn write_json_to_file(
         .read(true)
         .write(true)
         .create(true) // Create the file if it doesn't exist
-        .open(file_name_and_path)?;
+        .open(file_name_and_path)
+        .await?;
 
-    match file.write_all(serde_json::to_string_pretty(json_blob)?.as_bytes()) {
-        Ok(_) => println!("Successfully wrote JSON to file: {}", file_name_and_path),
-        Err(e) => println!("Failed to write JSON to file: {}. Error: {:?}", file_name_and_path, e),
-    }
+    file.write_all(serde_json::to_string_pretty(json_blob)?.as_bytes()).await?;
+    file.flush().await?;
+
+    println!("Successfully wrote JSON to file: {}", file_name_and_path);
 
     Ok(())
 }
 #[tokio::main]
 async fn main() {
-
     let my_json: Value = json!({
         "a": 123,
         "b": "hello"
