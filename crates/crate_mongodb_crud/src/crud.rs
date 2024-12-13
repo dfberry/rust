ecef7568b7edbc2878739e471352705ccedc1bc7
Dina Berry <dinaberry@outlook.com>
2024-12-13 02:39:29 +0000

    update to mongo v3

--- crates/crate_mongodb_crud/src/crud.rs
+++ crates/crate_mongodb_crud/src/crud.rs
@@ -11,38 +11,29 @@ pub async fn get_connection(connection_string: String) -> Client {
     return Client::with_uri_str(connection_string).await.unwrap()
 }
 
-pub async fn create<T: Serialize>(collection: &Collection<T>, item: &T) -> Result<String> {
-    let result = collection.insert_one(item, None).await?;
-    
-    // return id as string
+
+pub async fn create<T: Serialize + Send + Sync>(collection: &Collection<T>, item: &T) -> Result<String> {
+    let result = collection.insert_one(item).await?;
     Ok(result.inserted_id.as_object_id().unwrap().to_hex())
 }
-  
-pub async fn read<'a, T: DeserializeOwned + 'a>(collection: &Collection<T>, filter: Document) -> Result<Vec<T>> {
-    let mut cursor = collection.find(filter, None).await?;
+
+pub async fn read<'a, T: DeserializeOwned + Send + Sync + 'a>(collection: &Collection<T>, filter: Document) -> Result<Vec<T>> {
+    let mut cursor = collection.find(filter).await?;
     let mut results = Vec::new();
     while let Some(result) = cursor.next().await {
         results.push(result?);
     }
     Ok(results)
 }
+
 pub async fn read_by_id<T: DeserializeOwned + Send + Sync + Unpin>(collection: &Collection<T>, id: &str) -> Result<Option<T>> {
-    let object_id = ObjectId::from_str(&id).unwrap();
+    let object_id = ObjectId::from_str(id).unwrap();
     let filter = doc! { "_id": object_id };
-    collection.find_one(filter, None).await
+    collection.find_one(filter).await
 }
-pub async fn update<T: Serialize>(collection: &Collection<T>, filter: Document, update: Document) -> Result<()> {
-    let update_result = collection.update_one(filter, update, None).await?;
-    if update_result.modified_count == 1 {
-        Ok(())
-    } else {
-        Err(mongodb::error::Error::from(std::io::Error::new(std::io::ErrorKind::Other, "No document was updated")))
-    }
-}
-pub async fn update_by_id<T: Serialize>(collection: &Collection<T>, id: &str, update_document: Document) -> Result<()> {
-    let object_id = ObjectId::from_str(&id).unwrap();
-    let filter = doc! { "_id": object_id };
-    let update_result = collection.update_one(filter, update_document, None).await?;
+
+pub async fn update<T: Serialize + Send + Sync>(collection: &Collection<T>, filter: Document, update: Document) -> Result<()> {
+    let update_result = collection.update_one(filter, update,).await?;
     if update_result.modified_count == 1 {
         Ok(())
     } else {
@@ -50,18 +41,30 @@ pub async fn update_by_id<T: Serialize>(collection: &Collection<T>, id: &str, up
     }
 }
 
-pub async fn delete<T: Serialize>(collection: &Collection<T>, filter: Document) -> Result<()> {
-    let delete_result = collection.delete_one(filter, None).await?;
+pub async fn update_by_id<T: Serialize + Send + Sync>(collection: &Collection<T>, id: &str, update_document: Document) -> Result<()> {
+    let object_id = ObjectId::from_str(id).unwrap();
+    let filter = doc! { "_id": object_id };
+    let update_result = collection.update_one(filter, update_document).await?;
+    if update_result.modified_count == 1 {
+        Ok(())
+    } else {
+        Err(mongodb::error::Error::from(std::io::Error::new(std::io::ErrorKind::Other, "No document was updated")))
+    }
+}
+
+pub async fn delete<T: Serialize + Send + Sync>(collection: &Collection<T>, filter: Document) -> Result<()> {
+    let delete_result = collection.delete_one(filter).await?;
     if delete_result.deleted_count == 1 {
         Ok(())
     } else {
         Err(mongodb::error::Error::from(std::io::Error::new(std::io::ErrorKind::Other, "No document was deleted")))
     }
 }
-pub async fn delete_by_id<T: Serialize>(collection: &Collection<T>, id: &str) -> Result<()> {
-    let object_id = ObjectId::from_str(&id).unwrap();
+
+pub async fn delete_by_id<T: Serialize + Send + Sync>(collection: &Collection<T>, id: &str) -> Result<()> {
+    let object_id = ObjectId::from_str(id).unwrap();
     let filter = doc! { "_id": object_id };
-    let delete_result = collection.delete_one(filter, None).await?;
+    let delete_result = collection.delete_one(filter).await?;
     if delete_result.deleted_count == 1 {
         Ok(())
     } else {
