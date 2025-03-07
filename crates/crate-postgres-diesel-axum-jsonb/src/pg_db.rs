9fae324041045288fdf842410150518f7fc18170
Dina Berry <dinaberry@outlook.com>
2025-03-07 20:40:05 +0000

    fixed for all columns

--- crates/crate-postgres-diesel-axum-jsonb/src/pg_db.rs
+++ crates/crate-postgres-diesel-axum-jsonb/src/pg_db.rs
@@ -5,28 +5,38 @@ use diesel::prelude::*;
 use diesel::QueryableByName;
 use diesel::RunQueryDsl;
 use diesel::sql_query;
-use diesel::sql_types::{Text, Timestamp};
+use diesel::sql_types::{Text, Timestamp, Integer};
 
 use chrono::NaiveDateTime;
 use dotenvy::dotenv;
 use std::env;
 use tracing::info;
+
 #[derive(QueryableByName, Queryable, Serialize, Deserialize, Debug, Default)]
 pub struct CustomQueryResult {
-    #[diesel(sql_type = Text)]
-    disk_usage: String,
-    #[diesel(sql_type = Text)]
-    fork_count: String,
-    #[diesel(sql_type = Text)]
-    open_prs_count: String,
-    #[diesel(sql_type = Text)]
-    watches_count: String,
-    #[diesel(sql_type = Text)]
-    issues_count: String,
-    #[diesel(sql_type = Text)]
-    stars_count: String,
+    #[diesel(sql_type = Integer)]
+    disk_usage: i32,
+
+    #[diesel(sql_type = Integer)]
+    open_prs_count: i32,
+    #[diesel(sql_type = Integer)]
+    open_issues_count: i32,
+
+    #[diesel(sql_type = Integer)]
+    forks_count: i32,
+    #[diesel(sql_type = Integer)]
+    watchers_count: i32,
+    #[diesel(sql_type = Integer)]
+    stargazers_count: i32,
+
     #[diesel(sql_type = Timestamp)]
     created_at: NaiveDateTime,
+    #[diesel(sql_type = Timestamp)]
+    updated_at: NaiveDateTime,
+    #[diesel(sql_type = Timestamp)]
+    pushed_at: NaiveDateTime,
+    #[diesel(sql_type = Timestamp)]
+    log_time: NaiveDateTime,
 }
 
 pub fn execute_custom_query(connection: &mut PgConnection, org_repo: &str) -> Vec<CustomQueryResult> {
@@ -39,19 +49,22 @@ pub fn execute_custom_query(connection: &mut PgConnection, org_repo: &str) -> Ve
             FROM
                 public.osb_github_logfiles
             WHERE
-                org_repo = $1
+                org_repo = 'azure-samples/azure-typescript-e2e-apps'
             ORDER BY
                 created_at DESC
             LIMIT 30
         )
         SELECT
-            logfile ->> 'diskUsage' as "disk_usage",
-            logfile ->> 'forkCounts' as "fork_count",
-            logfile ->> 'openPRsCount' as "open_prs_count",
-            logfile ->> 'watchesCount' as "watches_count",
-            logfile ->> 'openIssuesCount' as "issues_count",
-            logfile ->> 'starsCount' as "stars_count",
-            created_at
+            (logfile ->> 'diskUsage')::int as "disk_usage",
+            (logfile ->> 'openPRsCount')::int as "open_prs_count",
+            (logfile ->> 'openIssuesCount')::int as "open_issues_count",
+            (logfile ->> 'forksCount')::int as "forks_count",
+            (logfile ->> 'watchersCount')::int as "watchers_count",
+            (logfile ->> 'stargazersCount')::int as "stargazers_count",
+            (logfile ->> 'createdAt')::timestamp as "created_at",
+            (logfile ->> 'updatedAt')::timestamp as "updated_at",
+            (logfile ->> 'pushedAt')::timestamp as "pushed_at",
+            created_at as "log_time"
         FROM T
         ORDER BY created_at DESC;
     "#;
