491d59a387087225b523224eafa4bd27dc67cef2
Dina Berry <dinaberry@outlook.com>
2025-03-28 12:43:08 +0000

    Fix jsonb deserialize

--- crates/crate-postgres-crud-jsonb-lib-deserialize/src/models.rs
+++ crates/crate-postgres-crud-jsonb-lib-deserialize/src/models.rs
@@ -10,12 +10,13 @@ use diesel::Insertable;
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 use chrono::NaiveDateTime;
-use serde_json::Value;
 
+#[derive(Queryable, Selectable, Serialize, Deserialize, Debug)]
 #[diesel(table_name = crate::schema::logfiles)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Logfile {
     pub id: Uuid,
+    #[diesel(sql_type = Jsonb)]
     pub logfile: Value,
     pub created_at: NaiveDateTime,
     pub org_repo: String,
